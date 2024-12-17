48cea6b5c0bd43918107e3bbb359aed959fa10a9
dave-mp <david.mendoza@dualbootpartners.com>
2024-12-17 14:19:31 -0500

    day 11 part 2 with cupy

--- 11/2/main.py
+++ 11/2/main.py
@@ -1,34 +1,46 @@
 import sys
-import numpy as np
-import itertools
-from tqdm import tqdm
+import cupy as cp
 
 
 def process_stone(stone):
-    if stone == "0":
-        return ["1"]
-    elif len(stone) % 2 == 0:
-        return [stone[: len(stone) // 2], f"{int(stone[len(stone) // 2 :])}"]
+    if cp.array(stone) == cp.array([0]):
+        return cp.array([cp.array([1]), cp.array([-1])], dtype=cp.int64)
+
     else:
-        return [f"{int(stone) * 2024}"]
+        num_str = cp.array2string(stone[0])
+        if len(num_str) % 2 == 0:
+            return cp.array(
+                [
+                    cp.array([cp.int64(num_str[: len(num_str) // 2])]),
+                    cp.array([cp.int64(num_str[len(num_str) // 2 :])]),
+                ],
+                dtype=cp.int64,
+            )
+
+        else:
+            new_stone_value = cp.multiply(cp.array(stone), cp.array([2024]))
+            return cp.array([new_stone_value, cp.array([-1])], dtype=cp.int64)
 
 
 def blink(line):
-    vectorized_func = np.frompyfunc(process_stone, 1, 1)
-    result = vectorized_func(np.array(line, str))
-    new_line = list(itertools.chain(*result))
+    result = cp.apply_along_axis(process_stone, 1, line)
+    chained_result = cp.concatenate(result)
+    new_line = cp.array(
+        cp.reshape(chained_result[chained_result != cp.array([-1])], (-1, 1)),
+        dtype=cp.int64,
+    )
 
     return new_line
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        line = sys.argv[1].split(" ")
+        line = cp.array([[int(x)] for x in sys.argv[1].split(" ")], dtype=cp.int64)
 
         for i in range(75):
-            print("BLINK ==>", i, "STONES ==>", len(line), end="\r")
+            print("BLINK ==>", i, "STONES ==>", len(line))
             line = blink(line)
 
-        print("STONES AFTER 25 BLINKS ==>", len(line))
+        print("STONES AFTER 75 BLINKS ==>", len(line))
     else:
         print("No input line provided")
--- 11/2/tests.py
+++ 11/2/tests.py
@@ -1,22 +1,33 @@
 import unittest
+import cupy as cp
 from main import blink, process_stone
 
 
 class TestDefragmentFile(unittest.TestCase):
     def test_process_stone_1(self):
-        assert process_stone("17") == ["1", "7"]
+        ps = process_stone([17])
+        assert cp.all(ps == cp.array([[1], [7]], dtype=cp.int64))
 
     def test_process_stone_2(self):
-        assert process_stone("2024") == ["20", "24"]
+        ps = process_stone([1])
+        assert cp.all(ps == cp.array([[2024], [-1]], dtype=cp.int64))
 
     def test_process_stone_3(self):
-        assert process_stone("125") == ["253000"]
+        ps = process_stone([125])
+        assert cp.all(ps == cp.array([[253000], [-1]], dtype=cp.int64))
+
+    def test_process_stone_4(self):
+        ps = process_stone([0])
+        assert cp.all(ps == cp.array([[1], [-1]], dtype=cp.int64))
 
     def test_blink_1(self):
-        assert blink(["125", "17"]) == ["253000", "1", "7"]
+        b = blink(cp.array([[125], [17], [17]], dtype=cp.int64))
+        print(b)
+        assert cp.all(b == cp.array([[253000], [1], [7], [1], [7]], dtype=cp.int64))
 
     def test_blink_2(self):
-        assert blink(["253000", "1", "7"]) == ["253", "0", "2024", "14168"]
+        b = blink(cp.array([[253000], [1], [7]], dtype=cp.int64))
+        assert cp.all(b == cp.array([[253], [0], [2024], [14168]], dtype=cp.int64))
 
 
 if __name__ == "__main__":
